633cc13c0f56d7cbdfd3702a38e04391119fd3f0
Diego Rosas <de.rosasgonzalez@ugto.mx>
2019-05-15 16:31:42 -0500

    CSS de nosotros casi listo. Falta la sección de miembros

--- src/components/nosotros.js
+++ src/components/nosotros.js
@@ -7,6 +7,7 @@ export default class Nosotros extends Component {
     render() {
         return (
             <div className="nosotros">
+
                 <h2>Sobre Nosotros</h2>
                 <div className="section1">
                     <img src="img/group.jpg" alt="group"/>
@@ -14,50 +15,75 @@ export default class Nosotros extends Component {
                     repellendus nisi commodi natus maiores enim fuga quae nostrum. 
                     </p>
                 </div>
+
                 <hr/>
-                <h2>Misión</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Quibusdam aliquam 
-                repellendus nisi commodi natus maiores enim fuga quae nostrum. 
-                </p>
-                <h2>Visión</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Quibusdam aliquam 
-                    repellendus nisi commodi natus maiores enim fuga quae nostrum. 
-                </p>
+
+                <div className="section2">
+                    <h2>Misión</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Quibusdam aliquam 
+                        repellendus nisi commodi natus maiores enim fuga quae nostrum. 
+                        </p>
+                    <h2>Visión</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Quibusdam aliquam 
+                        repellendus nisi commodi natus maiores enim fuga quae nostrum. 
+                        </p>
+                </div>
                 <hr/>
+
                 <h2>Nuestro equipo</h2>
                 <div className="equipo">
                     <div className="miembro">
-                        <img src="/img/man.jpg" alt="man"/>
-                        <h3>
-                            Presidente
-                        </h3>
-                        <p>Lorem ipsum dolor sit amet</p>
+                        <div className="miembro-imagen">
+                            <img src="/img/man.jpg" alt="man"/>
+                        </div>
+                        <div className="texto-miembro">
+                            <h3>
+                                Presidente
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet</p>
+                        </div>
                     </div>
                     <div className="miembro">
-                        <img src="/img/man.jpg" alt="man"/>
-                        <h3>
-                            Vicepresidente
-                        </h3>
-                        <p>Lorem ipsum dolor sit amet</p>
-                    </div>
-                    <div className="miembro">
-                        <img src="/img/man.jpg" alt="man"/>
-                        <h3>
+                        <div className="miembro-imagen">
+                            <img src="/img/man.jpg" alt="man"/>
+                        </div>
+                        <div className="texto-miembro">
+                            <h3>
                             Secretario
-                        </h3>
-                        <p>Lorem ipsum dolor sit amet</p>
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet</p>
+                        </div>
                     </div>
                     <div className="miembro">
-                        <img src="/img/man.jpg" alt="man"/>
-                        <h3>
-                            Tesorero
-                        </h3>
-                        <p>Lorem ipsum dolor sit amet</p>
+                        <div className="miembro-imagen">
+                            <img src="/img/man.jpg" alt="man"/>
+                        </div>
+                        <div className="texto-miembro">
+                            <h3>
+                                Tesorero
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet</p>
+                        </div>
+                    </div>
+                    <div className="miembro">
+                        <div className="miembro-imagen">
+                            <img src="/img/man.jpg" alt="man"/>
+                        </div>
+                        <div className="texto-miembro">
+                            <h3>
+                                Vicepresidente
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet</p>
+                        </div>
                     </div>
                 </div>
 
                 <h2>Bee Developers</h2>
-                <img className="grupo" src="/img/group.jpg" alt="group"/>
+                <div  className="grupo">
+                    <div className="grupo1">
+                        <img src="/img/group.jpg" alt="group"/>
+                    </div>
+                </div>
             </div>
         );
     }
