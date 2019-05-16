752d4c814b5d27c580058e33b1ba7ee7f2d26def
Diego Rosas <de.rosasgonzalez@ugto.mx>
2019-05-16 08:23:29 -0500

    diseno responsivo de home header y footer

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import {Link} from 'react-router-dom';
 import "./home.css";
 
 export default class Home extends Component {
@@ -62,6 +63,7 @@ export default class Home extends Component {
                     <img src="img/ruby.png" alt="ruby" />
                 </div>
             </div>
+            <Link to="/tallerescursos"><button className="boton-talleres">Talleres y Cursos ></button></Link>
           </div>
         );
     }
