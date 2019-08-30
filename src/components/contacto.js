8e80e07d88b8ba8bbfd0cb9c71d6ff95952db06e
Alan Alvarez <alanalvarez98.aa@gmail.com>
2019-08-30 00:05:01 -0500

    CSS y html de contacto Acrualizado

--- src/components/contacto.js
+++ src/components/contacto.js
@@ -6,58 +6,41 @@ export default class Contacto extends Component {
     render() {
         return (
           <div className="contacto">
-            <h2>Redes Sociales</h2>
-            <div className="botones">
-              <div className="facebook-youtube">
-                <div className="a-facebook">
-                  <a
-                    href="https://www.facebook.com/BeeDevelopersUG/"
-                    target="blank"
-                  >
-                    <button className="facebook">Facebook</button>
-                  </a>
-                </div>
-                <div className="a-youtube">
-                  <a
-                    href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-                    target="blank"
-                  >
-                    <button className="youtube">YouTube</button>
-                  </a>
-                </div>
-              </div>
-              <div className="instagram-twitter">
-                <div className="a-instagram">
-                  <a
-                    href="https://www.instagram.com/beedevelopers/?hl=es-la"
-                    target="blank"
-                  >
-                    <button className="instagram">Instagram</button>
-                  </a>
-                </div>
-                <div className="a-twitter">
-                  <a href="https://www.twitter.com/" target="blank">
-                    <button className="twitter">Twitter</button>
-                  </a>
-                </div>
-              </div>
-            </div>
+
             <div className="informacion">
               <h2>Contacto</h2>
-              <div className="items">
-                <p>Teléfono: 4646464646</p>
-                <p>Correo: b.dev@gmail.com</p>
-                <p>
-                  Domicilio: Oficinas de CEMER. Camino a Palo Blanco
-                  #227 Salamanca, Guanajuato.
-                </p>
-              </div>
+              <p>Teléfono: (464) 646-46-46</p>
+              <p>E-mail: b.dev@gmail.com</p>
+              <p>
+                Domicilio: Oficinas de CEMER. Camino a Palo Blanco
+                #227 Salamanca, Guanajuato.
+              </p>
             </div>
-            <div className="logo">
-              <img
-                src="img/logo_fclaro.png"
-                alt="Logo sobre fondo claro"
-              />
+
+            <div className="redes-sociales">
+              <h2>Redes Sociales</h2>
+              <div className="contacto-links">
+                <div className="facebook">
+                  <a href="https://www.facebook.com/BeeDevelopersUG/" target="blank">
+                    <i class="fab fa-facebook-square"></i>
+                  </a>
+                </div>
+                <div className="youtube">
+                  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="blank">
+                    <i class="fab fa-youtube"></i>
+                  </a>
+                </div>
+                <div className="instagram">
+                  <a href="https://www.instagram.com/beedevelopers/?hl=es-la" target="blank">
+                    <i class="fab fa-instagram"></i>
+                  </a>
+                </div>
+                <div className="twitter">
+                  <a href="https://www.twitter.com/" target="blank">
+                    <i class="fab fa-twitter-square"></i>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         );
