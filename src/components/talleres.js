cf598a5b1d2c23f3040d362189696d3822563ef0
Alan Alvarez <alanalvarez98.aa@gmail.com>
2019-08-30 15:33:45 -0500

    Test sección de talleres

--- src/components/talleres.js
+++ src/components/talleres.js
@@ -4,11 +4,14 @@ import './talleres.css';
 export default class Talleres extends Component {
 
     render() {
+
         return (
             <div className="talleres">
                 <h2>Semana de Open Day</h2>
                 <div className="talleres-formularios">
-                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfe1KD6SMkSyZnKjTMJ3aYKdARUbvYFQiAepors-98y3MB0zg/viewform?embedded=true" width="500" height="1338" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfe1KD6SMkSyZnKjTMJ3aYKdARUbvYFQiAepors-98y3MB0zg/viewform?embedded=true" width="380" height="1338" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+                   
+                    
                     
                     
                 </div>
