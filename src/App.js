11c59e38651edbefab2959d0f0c10b3ca9d28af6
Alan Alvarez <alanalvarez98.aa@gmail.com>
2019-09-05 20:48:29 -0500

    Rouert file

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
 import React,{Component} from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import Home from './components/home';
-import Nosotros from './components/nosotros';
-import Contacto from './components/contacto';
-import Talleres from './components/talleres';
-import Header from './components/header';
-import Error from './components/error';
 
 
 import Router from './components/router'
+
 
 
 export default class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-                <Header/>
-                <Switch>
-                    <Route exact path="/"component={Home} />
-                    <Route exact path="/nosotros"component={Nosotros} />
-                    <Route exact path="/tallerescursos"component={Talleres} />
-                    <Route exact path="/contacto"component={Contacto} />
-                    <Route component={Error}/>
-                </Switch>
-                <Header/>
-      </BrowserRouter>
+      <Router />
   );
   }
 }
-// export default class App extends Component {
-
-//   render() {
-//     return (
-//       <Router />
-//   );
-//   }
-// }
 
