b9d6f2fa4b4ae771a3bc2a03dc34b1f81982e3e8
Alan Alvarez <alanalvarez98.aa@gmail.com>
2020-05-11 11:28:21 -0500

    Creación de carpeta build

---
+++ build/precache-manifest.f3633d468a0760d275df786912df1470.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad4d56675993bf0233a1527c333b8d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1e4041bfd1ef91c499f",
+    "url": "/static/css/main.f1bf7000.chunk.css"
+  },
+  {
+    "revision": "a93d0b965437a0a2772e",
+    "url": "/static/js/2.593cffb8.chunk.js"
+  },
+  {
+    "revision": "f1e4041bfd1ef91c499f",
+    "url": "/static/js/main.d87b2ea5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
